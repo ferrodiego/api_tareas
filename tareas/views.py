a36d733e6601947459238c8efc31fd6bf42f9af4
ferrodiego <ferrodiego090@gmail.com>
2025-06-23 13:52:51 -0300

    API de tareas django con JWT

--- tareas/views.py
+++ tareas/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Tarea
 from .serializers import TareaSerelializer
 
@@ -6,3 +7,12 @@ from .serializers import TareaSerelializer
 class TareaViewset(viewsets.ModelViewSet):
     queryset = Tarea.objects.all()
     serializer_class = TareaSerelializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Tarea.objects.filter(usuario = self.request.user)
+    
+    def perform_create(self, serializer):
+        return serializer.save(usuario = self.request.user)
+    
+    
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import RegistroView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('registro/', RegistroView.as_view(), name = 'registro'),
+     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
